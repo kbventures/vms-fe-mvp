5fe1e15cfb546e12eb9230f240288ce8e3b80eb2
kbventures <kmb_x@hotmail.com>
2024-12-07 13:25:24 -0500

    React Component, Open Props and W3C V2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-import './components/Profile';  // Import the Lit component
+import './components/Profile';  // Import the Lit component (custom element)
+import Profile1 from './components/Profile1';  // Import React component
 
 function App() {
   return (
     <div className="App">
+      {/* Using the Lit web component directly in JSX */}
       <volunteer-profile></volunteer-profile>
+
+      {/* Using the React component */}
+      <Profile1 />
     </div>
   );
 }
